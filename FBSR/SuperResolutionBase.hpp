926cd487248668098479e661b3b735b4049407e9
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-04-10 09:16:46 +0800

    finish SuperResolutionBase Class

--- FBSR/SuperResolutionBase.hpp
+++ FBSR/SuperResolutionBase.hpp
@@ -13,26 +13,39 @@ class SuperResolutionBase
 {
 public:
 	bool SetFrameSource(const cv::Ptr<FrameSource>& frameSource);
-	bool reset();
+	bool Reset();
 
-	void nextFrame(OutputArray outputFrame);
+	void NextFrame(OutputArray outputFrame);
+
+protected:
+	virtual void Init(Ptr<FrameSource>& frameSource) = 0;
+	virtual void Process(Ptr<FrameSource>& frameSource, OutputArray output) = 0;
 
 private:
 	Ptr<FrameSource> frameSource;
-
 	bool isFirstRun;
 };
 
 bool SuperResolutionBase::SetFrameSource(const cv::Ptr<FrameSource>& frameSource)
 {
 	this->frameSource = frameSource;
-
+	this->isFirstRun = true;
 	return true;
 }
 
-bool SuperResolutionBase::reset()
+bool SuperResolutionBase::Reset()
 {
 	this->frameSource->reset();
-
+	this->isFirstRun = true;
 	return true;
+}
+
+void SuperResolutionBase::NextFrame(OutputArray outputFrame)
+{
+	if (isFirstRun)
+	{
+		Init(this->frameSource);
+		isFirstRun = false;
+	}
+	Process(this->frameSource, outputFrame);
 }
