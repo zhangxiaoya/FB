cf3ad37a564ec8184665d23f08ca4e7617859a54
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-04-10 09:40:22 +0800

    add MultiFrameProcessor Class

---
+++ FBSR/MultiFrameProcesser.hpp
@@ -0,0 +1,15 @@
+#include <opencv2\core\core.hpp>
+
+using namespace cv;
+
+class MultiFrameProcesser
+{
+protected:
+	void Process(OutputArray outputFrame);
+
+};
+
+void MultiFrameProcesser::Process(OutputArray ouputFrame)
+{
+	// do nothing now, will finish soon
+}
