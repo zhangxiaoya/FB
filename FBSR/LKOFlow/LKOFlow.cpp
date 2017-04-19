ee48bb02d547f59d433c28b0cc1245adebc14674
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-04-19 09:36:35 +0800

    fix meshgrid issue

--- FBSR/LKOFlow/LKOFlow.cpp
+++ FBSR/LKOFlow/LKOFlow.cpp
@@ -151,25 +151,21 @@ Mat LKOFlow::ResampleImg(Mat& img, Rect& rect, vector<double> disc)
 	auto leftTop = rect.tl();
 	auto bottomeRight = rect.br();
 
-	Meshgrid(Range(leftTop.x, bottomeRight.x - 1) - disc[0], Range(leftTop.y, bottomeRight.y - 1) - disc[1], X, Y);
-
-	Mat formatX, formatY;
-	X.convertTo(formatX, CV_32FC1);
-	Y.convertTo(formatY, CV_32FC1);
+	Meshgrid(leftTop.x - disc[0], bottomeRight.x - 1 - disc[0], leftTop.y - disc[1], bottomeRight.y - 1 - disc[1], X, Y);
 
 	Mat result;
-	remap(img, result, formatX, formatY, INTER_LINEAR);
+	remap(img, result, X, Y, INTER_LINEAR);
 
 	return result;
 }
 
-void LKOFlow::Meshgrid(const Range& xgv, const Range& ygv, Mat& X, Mat& Y)
+void LKOFlow::Meshgrid(const float lefTopX, const float rightBottomX, const float lefTopY, const float rightBottomY, Mat& X, Mat& Y)
 {
-	vector<int> t_x, t_y;
+	vector<float> t_x, t_y;
 
-	for (auto i = xgv.start; i <= xgv.end; i++)
+	for (auto i = lefTopX; i <= rightBottomX; i++)
 		t_x.push_back(i);
-	for (auto j = ygv.start; j <= ygv.end; j++)
+	for (auto j = lefTopY; j <= rightBottomY; j++)
 		t_y.push_back(j);
 
 	cv::repeat(cv::Mat(t_x).t(), t_y.size(), 1, X);
