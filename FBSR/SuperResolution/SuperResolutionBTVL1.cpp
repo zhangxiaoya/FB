b9a6c9a07ec1bed1bede1cb674426409d78762b9
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-04-26 17:06:36 +0800

    remove unused class

--- FBSR/SuperResolution/SuperResolutionBTVL1.cpp
+++
@@ -1,9 +0,0 @@
-#include "SuperResolutionBTVL1.h"
-
-void SuperResolutionBTVL1::Init(Ptr<FrameSource>& frameSource)
-{
-}
-
-void SuperResolutionBTVL1::Process(Ptr<FrameSource>& frameSource, OutputArray outputFrame)
-{
-}
--- FBSR/SuperResolution/SuperResolutionFactory.h
+++ FBSR/SuperResolution/SuperResolutionFactory.h
@@ -1,14 +1,9 @@
 #pragma once
-#include "SuperResolutionBTVL1.h"
 #include "SuperResolutionBase.h"
 
 class SuperResolutionFactory
 {
 public:
-	static Ptr<SuperResolutionBase> CreateSuperResolutionBTVL1()
-	{
-		return new SuperResolutionBTVL1();
-	}
 	static Ptr<SuperResolutionBase> CreateSuperResolutionBase()
 	{
 		return new SuperResolutionBase();
